cbacee1ad1f2c1dc050ba1a21eeb77cb759b821d
Nam <kaidn28@gmail.com>
2021-12-01 21:58:29 +0700

    管理者の画面、ユーザ編集のテーブル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  def after_sign_in_path_for(resource)
+    if current_user.is_admin
+      return '/admin'
+    end
+    return root_path
+  end
   protected
   def configure_permitted_parameters
     # sign_upのときに、name, ageを許可する

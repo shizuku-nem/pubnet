0821467c6c886ba01d4c1efde47bbe00c7d44167
Ubuntu <ubuntu@ip-172-31-6-76.ec2.internal>
2021-12-14 08:34:46 +0000

    fix edit profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     has_one_attached :image
-    has_many :post_comment, -> {order "created_at DESC"}
+    has_many :post_comment, -> {order "created_at ASC"}
     belongs_to:user
 end
